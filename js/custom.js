43797a342bb375cf7dd9c35bfb68a64cf90e75d2
Mykola Ivanov <offmykolaivanov@gmail.com>
2019-10-28 13:28:28 +0200

    added preload

--- js/custom.js
+++ js/custom.js
@@ -1,23 +1,46 @@
 window.addEventListener('load', () => {
 	const preload = document.getElementById('preload');
-	const preloadText = preload.querySelector('.text');
+	const resultBox = document.getElementById('result_box');
+	const select = document.getElementById('select');
 
-	HTMLElement.prototype.hide = function() {
-		this.classList.add('hiding');
+	HTMLElement.prototype.hide = function(text) {
+		this.querySelector('.text').innerHTML = text;
 		setTimeout(() => {
-			this.classList.add('hide');
+			if ( this.className === 'show' ) {
+				this.className = '';
+				return;
+			}
+			this.classList.add('hiding');
+			setTimeout(() => {
+				if ( this.className === 'show' ) {
+					this.className = '';
+					return;
+				}
+				this.classList.add('hide');
+			}, 500);
 		}, 500);
 	};
 
-	HTMLElement.prototype.show = function() {
-		this.className = '';
+	HTMLElement.prototype.show = function(text) {
+		this.querySelector('.text').innerHTML = text;
+		if (this.classList.contains('hiding')) this.className = 'show';
+
 	};
 
-	preload.hide();
+	function renderSelect(phoneDB) {
+		const uniquePhones = [];
+		let selectHtml = '<option value="">All</option>';
+		phoneDB.forEach((item) => {
+			if ( !uniquePhones.includes( item.Brand ) ) {
+				uniquePhones.push( item.Brand );
+				selectHtml += '<option value="' + item.Brand + '">' + item.Brand + '</option>';
+			}
+		});
+		select.innerHTML = selectHtml;
+	}
 
 	// search option
 	const options = {
-		shouldSort: true,
 		threshold: 0.6,
 		location: 0,
 		distance: 100,
@@ -54,16 +77,30 @@ window.addEventListener('load', () => {
 	let db;
 
 	(async function() {
-		preloadText.innerText = 'Load phone DB';
+		preload.show('Load phone DB');
 		const dbResponse = await fetch('phones/phones.json');
 		if ( dbResponse.ok ) {
 			db = await dbResponse.json();
-			console.log(db);
+			preload.hide('Load done');
+			renderSelect(db);
 		} else {
 			console.log('DB error:', dbResponse.status);
+			preload.show('Load phone DB error!');
 		}
 	})();
 
-	// var fuse = new Fuse(list, options); // "list" is the item array
-	// var result = fuse.search("");
+	function renderResult(result) {
+		if ( result.length ) {
+			let renderHTML = '';
+			if ( result.length > 100 ) renderHTML += '<p>Result too many (>100) shwo only first 100 elements.</p>'
+		} else {
+			resultBox.innerHTML = '<p>Results not found!</p>'
+		}
+	}
+
+	document.getElementById('search').addEventListener('keyup', function() {
+		const fuse = new Fuse(db, options);
+		const result = fuse.search(this.value);
+		renderResult(result);
+	});
 });
